3f8e12ccb40972f21c9ec96f10ec670db8872505
Bolado <vtcb@cin.ufpe.br>
2017-12-06 15:49:36 -0300

    Improve encoder matching

--- src/LZ77_encoder.cc
+++ src/LZ77_encoder.cc
@@ -3,6 +3,9 @@
 #include <string>
 #include <tuple>
 #include <utility>
+#include <vector>
+
+#include "debug.h"
 
 #define LZ77_0 '%'
 #define LZ77_1 '$'
@@ -61,20 +64,51 @@ std::string LZ77Encoder::decode(const std::string& code) {
 std::pair<unsigned int, unsigned int>
     LZ77Encoder::bestMatch(const std::string& text, unsigned int at) {
   unsigned int begin = at <= buffer_size ? 0 : at - buffer_size;
+  unsigned int end   = at + buffer_size >= text.size() ? text.size() : at + buffer_size;
   unsigned int best_length = 0;
   unsigned int best_offset = 0;
-  for (unsigned int i = begin; i < at; i++) {
-    unsigned int cur_length = 0;
-    while (cur_length < buffer_size
-        && at + cur_length < text.size()
-        && text[i + cur_length] == text[at + cur_length]) {
-        cur_length++;
+
+  std::vector<int> table(end - at + 1);
+  unsigned int position = 0;
+  int candidate = -1;
+  table[0] = -1;
+
+  while (position < end - at) {
+    while (candidate >= 0 && text[at + position] != text[at + candidate]) {
+      candidate = table[candidate];
     }
-    if (cur_length > best_length) {
-      best_length = cur_length;
-      best_offset = at - i;
+    candidate++;
+    position++;
+    table[position] = candidate;
+  }
+
+  std::vector<std::vector<int>> fail(256, table);
+  for (int i = 0; i < 256; i++) {
+    for (unsigned int j = 0; j < table.size(); j++) {
+      unsigned char ch = i;
+      int dj = j;
+      while (dj >= 0 && ch != static_cast<unsigned char>(text[at + dj])) {
+        dj = table[dj];
+      }
+      fail[i][j] = dj;
     }
   }
+
+  int i = 0, j = 0;
+  int n = at - begin, m = end - at;
+
+  while (i < n) {
+    j = fail[static_cast<unsigned char>(text[begin + i])][j];
+    i++; j++;
+    if (j > static_cast<int>(best_length)) {
+      best_length = j;
+      best_offset = at - begin - i;
+    }
+    if (j == m) {
+      j = table[j];
+    }
+  }
+
   return std::make_pair(best_length, best_offset);
 }
 
--- src/LZ77_encoder.h
+++ src/LZ77_encoder.h
@@ -8,7 +8,7 @@
 
 class LZ77Encoder : public Encoder {
 public:
-  LZ77Encoder(unsigned int buffer_size = 256) : buffer_size(buffer_size) {}
+  LZ77Encoder(unsigned int buffer_size = 2048) : buffer_size(buffer_size) {}
   std::string encode(const std::string& text);
   std::string decode(const std::string& code);
   std::string magic() const;
